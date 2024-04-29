fc95794311befe167eb9cf45056753b8384eec69
Valentyn <sonik2001www@gmail.com>
2024-04-29 12:09:23 +0300

    chenged func

--- my_func.py
+++ my_func.py
@@ -1,3 +1,3 @@
 
 def sort_str(text: str) -> str: # ---------------------------------------------
-    return text.strip().replace("  ", " ").capitalize()
+    return text.strip().replace("  ", " ").capitalize() # ---------------------
